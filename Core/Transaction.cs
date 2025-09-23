79ba941da748fc6a342790327a2b7b15f23abb31
LisaEbbhagen <lisa.ebbhagen@gmail.com>
2025-09-23 16:24:50 +0200

    har skapat properties för transaction: id, accpuntnumber, amount, timestamp, type samt en konstruktor för dessa

--- Core/Transaction.cs
+++ Core/Transaction.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace K1_Banken_Team1
 {
-    internal class Transaction
+    public class Transaction
     {
+        public string Id { get; set; }
+        public string AccountNumber { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime Timestamp { get; set; }
+        public string Type { get; set; } //Val av type ska vara:
+                                         //“Deposit”/“Withdraw”/“Transfer”
+        
+        public Transaction (string id, string accountNumber, decimal amount, DateTime timestamp, string type)
+        {
+            Id = id;
+            AccountNumber = accountNumber;
+            Amount = amount;
+            Timestamp = timestamp;
+            Type = type;
+        }
     }
 }
