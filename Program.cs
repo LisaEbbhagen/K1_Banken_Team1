1e664f43f2516017e20c2fb498684c48b6d91057
LisaEbbhagen <lisa.ebbhagen@gmail.com>
2025-09-23 14:16:29 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace K1_Banken_Team1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
