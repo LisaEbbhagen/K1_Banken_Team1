bd35d52c5dc57e48bec89d548640ea0dab6047ad
LisaEbbhagen <lisa.ebbhagen@gmail.com>
2025-09-23 15:14:42 +0200

    la till klassen bank enligt instruktion

---
+++ Bank.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace K1_Banken_Team1
+{
+    internal class Bank
+    {
+    }
+}
