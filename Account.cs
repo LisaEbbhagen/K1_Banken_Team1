6bd2b937d6e742c2bc665393a97bc2bf1082ab30
Abdalle Abdulkadir <abdalle.abdulkadir@chasacademy.se>
2025-09-23 16:45:56 +0200

    Abdalle/ kika

--- Account.cs
+++ Account.cs
@@ -6,7 +6,42 @@ using System.Threading.Tasks;
 
 namespace K1_Banken_Team1
 {
-    internal class Account
+    public class Account
     {
+        public string AccountNumber { get; set; }
+        public Decimal Balance { get; private set; }
+        public User Owner {get; private set;}
+    }
+
+    public Account(string accountNumber, User owner)
+        {
+            AccountNumber = accountNumber;
+            Owner = owner;
+            Balance = 0m;
+        }
+        public bool Deposit(decimal amount)
+        {
+            if (amount <= 0)
+            { 
+              Console.WriteLine("enter an amount to deposit ");
+            string input = Console.ReadLine();
+
+            if (!decimal.TryParse(input, out decimal amount))
+            {
+                Console.WriteLine("Wrong: Invalid amount.");
+                return;
+            }
+
+            if (amount <= 0)
+            {
+                Console.WriteLine("Wrong: The amount must be greater then zero");
+                return;
+            }
+
+            Balance += amount;
+            Console.WriteLine($"✅ {amount} kr Deposited. New Balance: {Balance} kr.");
+        }
+
+
     }
 }
--- User.cs
+++ User.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace K1_Banken_Team1
 {
-    internal class User
+    public class User
     {
+    }
+
+    internal class Account
+    {
+        public string AccountNumber { get; set; }
+        public decimal Balance { get; set; }
+        public User Owner { get; set; }
+
+        public Account(string accountNumber, User owner)
+        {
+            AccountNumber = accountNumber;
+            Balance = 0;
+            Owner = owner;
+        }
+
+        public void Deposit(decimal amount)
+        {
+            Balance += amount;
+        }
     }
 }
