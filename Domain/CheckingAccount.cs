a12e1ac61b616aa8cbda77d9c9b40461ab9e7e5e
LisaEbbhagen <lisa.ebbhagen@gmail.com>
2025-11-03 13:02:48 +0100

    InspectCode/LE

--- Domain/CheckingAccount.cs
+++ Domain/CheckingAccount.cs
@@ -10,36 +10,5 @@ namespace K1_Banken_Team1.Domain
     public class CheckingAccount : Account
     {
         public CheckingAccount(string accountNumber, User owner) : base(accountNumber, owner) { }
-
-        public override bool Deposit(decimal amount, bool verbose = true) //metod för att sätta in pengar.
-        {
-            if (amount <= 0)
-            {
-                Console.WriteLine("Error: Summan du väljer att sätta in måste vara mer än 0kr.");
-                return false;
-            }
-
-            else
-            {
-                Balance += amount;
-                Console.WriteLine($"Aktuellt saldo efter insättning: {Balance}");
-                return true;
-            }
-        }
-
-        public override bool Withdraw(decimal amount, bool verbose = true) //metod för att ta ut pengar.
-        {
-            if (amount <= 0 || amount > Balance)
-            {
-                Console.WriteLine("Error: Summan du väljer att sätta in måste vara mer än 0kr.");
-                return false;
-            }
-            else
-            {
-                Balance -= amount;
-                Console.WriteLine($"Aktuellt saldo efter uttag: {Balance}");
-                return true;
-            }
-        }
     }
 }
